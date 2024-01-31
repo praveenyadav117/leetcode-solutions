b59bbcab5c2d4e9355c1876db68d7f1676b29a24
Praveen Kumar <58669290+praveenyadav117@users.noreply.github.com>
2024-01-31 10:04:06 +0530

    Time: 1 ms (99.63%) | Memory: 45 MB (61.88%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+        ans.add(
+            i % 15 == 0 ? "FizzBuzz":
+            i % 3 == 0 ? "Fizz":
+            i % 5 ==0 ? "Buzz":
+            String.valueOf(i)
+        );
+    }
+    return ans;
+        
+    }
+}
