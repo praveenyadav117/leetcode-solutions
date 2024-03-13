31d6278efe23e593fa62cd265d176a8c82598ada
Praveen Kumar <58669290+praveenyadav117@users.noreply.github.com>
2024-03-13 11:19:21 +0530

    Time: 1 ms (99.51%) | Memory: 57 MB (45.56%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -12,5 +12,15 @@ class Solution {
             }
         }
         return res;
+        // int res = Integer.MIN_VALUE;
+        // int sum = 0;
+        // for(int i=0;i<nums.length;i++){
+        //     sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         res = Math.max(sum,res);
+        //     }
+        // }
+        // return res;
     }
 }
